c59008e6a27c82e74083b55c2bb41ec6bdbc7df6
lukaszgrot <lugrot@gmail.com>
2022-03-17 23:27:43 +0100

    Payroll app

--- src/Infrastructure/Repository/DepartmentRepository.php
+++ src/Infrastructure/Repository/DepartmentRepository.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Repository;
 
+use App\Domain\Entity\Department;
 use App\Domain\Repository\DepartmentRepositoryInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
-class DepartmentRepository implements DepartmentRepositoryInterface
+class DepartmentRepository extends ServiceEntityRepository implements DepartmentRepositoryInterface
 {
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Department::class);
+    }
 }
